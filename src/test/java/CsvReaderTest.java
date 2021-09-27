b24b983db1f3d207f299fdf117fba057488f1f41
sandromax <sandrondk@gmail.com>
2021-09-27 23:39:41 +0300

    add method shouldThrowIOException in CsvReaderTest

--- src/test/java/CsvReaderTest.java
+++ src/test/java/CsvReaderTest.java
@@ -3,14 +3,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CsvReaderTest {
 
+    public static List<String[]> expected = new ArrayList<>() {{
+        add(new String[]{"id", "name"});
+        add(new String[]{"1", "Marketing"});
+        add(new String[]{"2", "Management"});
+        add(new String[]{"3", "Finance"});
+        add(new String[]{"4", "Production"});
+    }};
+
     @Test
-    public void shouldThrowException() {
+    public void shouldThrowIOException() {
 
         assertThrows(IOException.class, () -> CsvReader.read("noname.csv"));
 
     }
+
+    @Test
+    public void shouldReadTextFromFile() {
+
+        List<String[]> actual = new ArrayList<>();
+
+        try {
+            actual = CsvReader.read("src/main/resources/Test_Task.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < actual.size(); i++) {
+            assertArrayEquals(expected.get(i), actual.get(i));
+        }
+
+
+    }
+
 
 }
